c216e6dfcb5041106b182f826f342a44cf225412
Woojae Jeong <woo6388@naver.com>
2024-12-27 15:01:35 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/PetBridge/PetBridgeApplication.java
@@ -0,0 +1,13 @@
+package PetBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBridgeApplication.class, args);
+	}
+
+}
