7c2661f229339c631cff8dd14bcf6846c9e2daf3
Woojae Jeong <woo6388@naver.com>
2025-01-07 18:55:20 +0900

    feat:@ValidMember 어노테이션 구현

---
+++ src/main/java/PetBridge/auth/jwt/annotation/ValidMemberResolver.java
@@ -0,0 +1,40 @@
+package PetBridge.auth.jwt.annotation;
+
+import PetBridge.auth.jwt.service.JwtTokenProviderService;
+import PetBridge.member.model.entity.Member;
+import PetBridge.member.service.MemberService;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class ValidMemberResolver implements HandlerMethodArgumentResolver {
+    private final MemberService memberService;
+    private final JwtTokenProviderService jwtTokenProviderService;
+
+    public ValidMemberResolver(MemberService memberService, JwtTokenProviderService jwtTokenProviderService) {
+        this.memberService = memberService;
+        this.jwtTokenProviderService = jwtTokenProviderService;
+    }
+
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Member.class) &&
+                parameter.hasParameterAnnotation(ValidMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        String token = jwtTokenProviderService.extractToken(webRequest.getHeader(HttpHeaders.AUTHORIZATION)); //Http 헤더에서 토큰 추출
+        Long memberId = jwtTokenProviderService.parseClaims(token).get("id", Long.class); //토큰에 들어있는 멤버Id값 추출
+        return memberService.findByIdOrThrow(memberId);
+    }
+}
