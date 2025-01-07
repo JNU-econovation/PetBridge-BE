1643f232d51aea08e4ca2c5cd09b001f6cd16343
Woojae Jeong <woo6388@naver.com>
2025-01-07 18:55:01 +0900

    chore: 어노테이션 추가

--- src/main/java/PetBridge/PetBridgeApplication.java
+++ src/main/java/PetBridge/PetBridgeApplication.java
@@ -2,8 +2,10 @@ package PetBridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PetBridgeApplication {
 
 	public static void main(String[] args) {
