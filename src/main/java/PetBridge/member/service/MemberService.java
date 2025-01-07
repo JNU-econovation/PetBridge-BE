871ba38cb3b0de1bc558f6dc44a85cf99efe2a2e
Woojae Jeong <woo6388@naver.com>
2025-01-07 18:48:07 +0900

    feat: 멤버 엔티티 및 서비스 구현

---
+++ src/main/java/PetBridge/member/service/MemberService.java
@@ -0,0 +1,45 @@
+package PetBridge.member.service;
+
+import PetBridge.auth.dto.request.LoginReq;
+import PetBridge.auth.dto.request.SignUpReq;
+import PetBridge.auth.exception.businessException.AlreadySignedEmailException;
+import PetBridge.member.exception.BadRequest.MemberNotFoundException;
+import PetBridge.member.exception.BadRequest.InvalidEmailOrPasswordException;
+import PetBridge.member.model.entity.Member;
+import PetBridge.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public Member findByIdOrThrow(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
+    }
+
+    @Transactional(readOnly = true)
+    public void validateDuplicateEmail(String email) {
+        Boolean existByEmail = memberRepository.existsByEmail(email);
+        if (existByEmail)
+            throw new AlreadySignedEmailException();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findByEmailAndPasswordOrThrow(LoginReq loginReqDTO) {
+        return memberRepository.findByEmailAndPassword(loginReqDTO.email(), loginReqDTO.password())
+                .orElseThrow(InvalidEmailOrPasswordException::new);
+    }
+
+    @Transactional
+    public void createMember(SignUpReq signUpRequest) {
+        validateDuplicateEmail(signUpRequest.email());
+        memberRepository.save(signUpRequest.toEntity());
+    }
+}
