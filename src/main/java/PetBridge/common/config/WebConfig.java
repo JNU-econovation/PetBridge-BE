e6253ef58141e176d0ad56a7895f362b4c9e4e02
Woojae Jeong <woo6388@naver.com>
2025-01-07 19:45:11 +0900

    refactor: ValidMemberResolver 등록

--- src/main/java/PetBridge/common/config/WebConfig.java
+++ src/main/java/PetBridge/common/config/WebConfig.java
@@ -1,16 +1,22 @@
 package PetBridge.common.config;
 
+import PetBridge.auth.jwt.annotation.ValidMemberResolver;
 import PetBridge.common.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final JwtInterceptor jwtInterceptor;
+    private final ValidMemberResolver validMemberResolver;
 
-    public WebConfig(JwtInterceptor jwtInterceptor) {
+    public WebConfig(JwtInterceptor jwtInterceptor, ValidMemberResolver validMemberResolver) {
         this.jwtInterceptor = jwtInterceptor;
+        this.validMemberResolver = validMemberResolver;
     }
 
     @Override
@@ -18,5 +24,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/api/**")  // 인터셉터를 적용할 URL 패턴
                 .excludePathPatterns("/api/v1/auth/login", "/api/v1/auth/signup");  // 예외 URL
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(validMemberResolver);  // ArgumentResolver 등록
     }
 }
