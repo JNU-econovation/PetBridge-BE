4d4535b03e8007b29a6e9414a9fccfa7ed3aec11
Woojae Jeong <woo6388@naver.com>
2025-01-09 13:58:00 +0900

    refactor: InvalidEmailOrPasswordException 예외 HttpStatus 변경

--- src/main/java/PetBridge/member/exception/MemberErrorType.java
+++ src/main/java/PetBridge/member/exception/MemberErrorType.java
@@ -4,8 +4,8 @@ import PetBridge.common.exception.ErrorType;
 import org.springframework.http.HttpStatus;
 
 public enum MemberErrorType implements ErrorType {
-    MEMBER_NOT_FOUND_EXCEPTION ("Member400_001", HttpStatus.NOT_FOUND, "해당하는 멤버를 찾을 수 없습니다"),
-    INVALID_EMAIL_OR_PASSWORD_EXCEPTION("Member400_002",HttpStatus.NOT_FOUND, "이메일이나 비밀번호가 틀렸습니다");
+    MEMBER_NOT_FOUND_EXCEPTION ("Member404_001", HttpStatus.NOT_FOUND, "해당하는 멤버를 찾을 수 없습니다"),
+    INVALID_EMAIL_OR_PASSWORD_EXCEPTION("Member400_001",HttpStatus.BAD_REQUEST, "이메일이나 비밀번호가 틀렸습니다");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
