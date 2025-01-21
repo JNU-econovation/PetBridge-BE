807eed50117c6d52bd8843a6d7ee4523ee5f0ddc
Woojae Jeong <woo6388@naver.com>
2025-01-21 18:02:53 +0900

    refactor: 좋아요 수, 조회수 동시성 제어를 DB에서 하도록 수정

--- src/main/java/PetBridge/adoptionPost/repository/AdoptionPostRepository.java
+++ src/main/java/PetBridge/adoptionPost/repository/AdoptionPostRepository.java
@@ -2,21 +2,28 @@ package PetBridge.adoptionPost.repository;
 
 import PetBridge.adoptionPost.model.entity.AdoptionPost;
 import PetBridge.member.model.entity.Member;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface AdoptionPostRepository extends JpaRepository<AdoptionPost, Long> {
+public interface AdoptionPostRepository extends JpaRepository<AdoptionPost, Long>, AdoptionPostCustom{
 
-    // 최신순 (ID를 기준으로 내림차순 정렬)
-    Slice<AdoptionPost> findAllByOrderByIdDesc(Pageable pageable);
-    // 조회수 순 (조회수를 기준으로 내림차순 정렬)
-    Slice<AdoptionPost> findAllByOrderByClickCountDesc(Pageable pageable);
-    // 찜수 순 (찜수를 기준으로 내림차순 정렬)
-    Slice<AdoptionPost> findAllByOrderByWishCountDesc(Pageable pageable);
+    Optional<AdoptionPost> findByIdAndMember(Long postId, Member member);
 
+    @Modifying
+    @Query("UPDATE AdoptionPost a SET a.clickCount = a.clickCount + 1 WHERE a = :adoptionPost")
+    void incrementClickCount(@Param("adoptionPost") AdoptionPost adoptionPost);
+
+    @Modifying
+    @Query("UPDATE AdoptionPost a SET a.wishCount = a.wishCount -1 WHERE a = :adoptionPost")
+    void decrementWishCount(@Param("adoptionPost")AdoptionPost adoptionPost);
+
+    @Modifying
+    @Query("UPDATE AdoptionPost a SET a.wishCount = a.wishCount + 1 WHERE a = :adoptionPost")
+    void incrementWishCount(@Param("adoptionPost")AdoptionPost adoptionPost);
 }
