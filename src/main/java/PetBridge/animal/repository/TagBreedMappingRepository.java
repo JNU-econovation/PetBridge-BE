4f97b974a9f35aeb2348f8a8b88160fd30dc81b5
Woojae Jeong <woo6388@naver.com>
2025-01-21 18:07:52 +0900

    refactor: 태그와 품종간의 매핑 정보는 불필요해서 삭제

--- src/main/java/PetBridge/animal/repository/TagBreedMappingRepository.java
+++
@@ -1,7 +0,0 @@
-package PetBridge.animal.repository;
-
-import PetBridge.animal.model.entity.TagBreedMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagBreedMappingRepository extends JpaRepository<TagBreedMapping, Long> {
-}
